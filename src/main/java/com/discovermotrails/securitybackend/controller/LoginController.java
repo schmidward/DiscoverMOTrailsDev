e9ae6c491e5f10bf692b4adbcc8796f59af1892f
schmidward <eric.d.schmid1@gmail.com>
2023-05-01 19:53:14 -0500

    PostMapping('/register') working

--- src/main/java/com/discovermotrails/securitybackend/controller/LoginController.java
+++ src/main/java/com/discovermotrails/securitybackend/controller/LoginController.java
@@ -1,14 +1,38 @@
 package com.discovermotrails.securitybackend.controller;
 
+import com.discovermotrails.securitybackend.model.User;
+import com.discovermotrails.securitybackend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LoginController {
 
+    @Autowired
+    UserRepository userRepository;
 
     @PostMapping("/register")
-    public String processRegisterUser(){
-        return "You're registered";
+    public ResponseEntity processRegisterUser(@RequestBody User user){
+        System.out.println("\n ***USER POST request to add a user to the backend database");
+        User savedUser = null;
+        ResponseEntity response = null;
+        try {
+            savedUser = userRepository.save(user);
+
+            if (savedUser.getId() > 0) {
+                response = ResponseEntity
+                        .status(HttpStatus.CREATED)
+                        .body("Given user details are successfully created");
+            }
+        } catch (Exception ex) {
+            response = ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("An exception occurred due to " + ex.getMessage());
+        }
+        return response;
     }
 }
