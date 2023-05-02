653e37e3bfdb788ee5f11832b9e546e5ee2ff8e8
schmidward <eric.d.schmid1@gmail.com>
2023-05-01 19:56:43 -0500

    Password hashing working

--- src/main/java/com/discovermotrails/securitybackend/config/ProjectSecurityConfig.java
+++ src/main/java/com/discovermotrails/securitybackend/config/ProjectSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
@@ -33,7 +34,7 @@ public class ProjectSecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 
 }
--- src/main/java/com/discovermotrails/securitybackend/controller/LoginController.java
+++ src/main/java/com/discovermotrails/securitybackend/controller/LoginController.java
@@ -5,6 +5,7 @@ import com.discovermotrails.securitybackend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,12 +16,17 @@ public class LoginController {
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @PostMapping("/register")
     public ResponseEntity processRegisterUser(@RequestBody User user){
         System.out.println("\n ***USER POST request to add a user to the backend database");
         User savedUser = null;
         ResponseEntity response = null;
         try {
+            String hashPwd = passwordEncoder.encode(user.getPwd());
+            user.setPwd(hashPwd);
             savedUser = userRepository.save(user);
 
             if (savedUser.getId() > 0) {
