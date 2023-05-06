5a76acdfa9f8f6e0c83b68ba68798a9b7c7b956d
Eric D. Schmid <eric.d.schmid1@gmail.com>
2023-05-06 17:44:35 -0500

    more procrastinating making this header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './App.css'
 
 import Home from './pages/home.js';
 import Login from './pages/login.js';
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,23 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
+import './header.css'
 
 function Header() {
     return (
         <header>
-            <h1>DiscoverMOTrails</h1>
-            <nav>
-                <ul>
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/login">Login</Link>
-                    </li>
-                    <li>
-                        <Link to="/Register">Register</Link>
-                    </li>
-                </ul>
-            </nav>
+            <div className='header-container'>
+            <div className='logo'>
+                <Link to="/"><h1>DiscoverMOTrails</h1></Link>
+            </div>
+                <nav>
+                    <ul className='nav-links'>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/login">Login</Link>
+                        </li>
+                        <li>
+                            <Link to="/Register">Register</Link>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
         </header>
     );
 }
