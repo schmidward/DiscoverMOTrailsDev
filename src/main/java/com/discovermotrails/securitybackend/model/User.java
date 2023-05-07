00f70640627695da863cdadff69d722c006485d6
schmidward <eric.d.schmid1@gmail.com>
2023-05-06 20:41:41 -0500

    adding a single user and storing our login information to remember for the future.

--- src/main/java/com/discovermotrails/securitybackend/model/User.java
+++ src/main/java/com/discovermotrails/securitybackend/model/User.java
@@ -15,7 +15,6 @@ public class User {
     @GeneratedValue(strategy= GenerationType.AUTO, generator="native")
     @GenericGenerator(name = "native",strategy = "native")
     private int id;
-    private String username;
     private String email;
     private String pwd;
     private String role;
@@ -26,14 +25,6 @@ public class User {
 
     public void setId(int id) {
         this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
     }
 
     public String getEmail() {
