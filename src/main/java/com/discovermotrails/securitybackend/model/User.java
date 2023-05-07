525ef02d6f913d17af041c93c1f43d3a0b8a1f80
Eric D. Schmid <eric.d.schmid1@gmail.com>
2023-05-06 20:07:05 -0500

    Small change to the User model

--- src/main/java/com/discovermotrails/securitybackend/model/User.java
+++ src/main/java/com/discovermotrails/securitybackend/model/User.java
@@ -15,6 +15,7 @@ public class User {
     @GeneratedValue(strategy= GenerationType.AUTO, generator="native")
     @GenericGenerator(name = "native",strategy = "native")
     private int id;
+    private String username;
     private String email;
     private String pwd;
     private String role;
@@ -25,6 +26,14 @@ public class User {
 
     public void setId(int id) {
         this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public String getEmail() {
