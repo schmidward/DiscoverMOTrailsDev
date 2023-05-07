6dc90d1e9ce7a84412ed209c34e24f58666fb65f
schmidward <eric.d.schmid1@gmail.com>
2023-05-06 20:36:53 -0500

    Adding CORS

--- src/main/java/com/discovermotrails/securitybackend/config/ProjectSecurityConfig.java
+++ src/main/java/com/discovermotrails/securitybackend/config/ProjectSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.discovermotrails.securitybackend.config;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,8 +10,11 @@ import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 import javax.sql.DataSource;
+import java.util.Collections;
 
 @Configuration
 public class ProjectSecurityConfig {
@@ -19,18 +23,27 @@ public class ProjectSecurityConfig {
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
 
         //csrf disabled here to make basic things work for now. Will need to properly implement into the future
-        http.csrf().disable()
-                .authorizeHttpRequests().requestMatchers("/account").authenticated()
+        http.cors().configurationSource(new CorsConfigurationSource() {
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                        CorsConfiguration config = new CorsConfiguration();
+                        config.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+                        config.setAllowedMethods(Collections.singletonList("*"));
+                        config.setAllowCredentials(true);
+                        config.setAllowedHeaders(Collections.singletonList("*"));
+                        config.setMaxAge(3600L);
+                        return config;
+                    }
+                })
+                .and().csrf().disable()
+                .authorizeHttpRequests().requestMatchers("/account", "/secure").authenticated()
                 .requestMatchers("/index", "/register").permitAll()
                 .and().formLogin()
                 .and().httpBasic();
         return http.build();
     }
 
-//    @Bean
-//    public UserDetailsService userDetailsService(DataSource dataSource) {
-//        return new JdbcUserDetailsManager(dataSource);
-//    }
+
 
     @Bean
     public PasswordEncoder passwordEncoder() {
