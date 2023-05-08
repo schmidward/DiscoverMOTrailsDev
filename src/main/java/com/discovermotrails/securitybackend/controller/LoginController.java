5ed438e259db463e4768db0d41d956f32d8c6d36
schmidward <eric.d.schmid1@gmail.com>
2023-05-08 08:55:53 -0500

    minor updates

--- src/main/java/com/discovermotrails/securitybackend/controller/LoginController.java
+++ src/main/java/com/discovermotrails/securitybackend/controller/LoginController.java
@@ -1,22 +1,14 @@
 package com.discovermotrails.securitybackend.controller;
 
-import com.discovermotrails.securitybackend.config.UserDetails;
 import com.discovermotrails.securitybackend.model.User;
 import com.discovermotrails.securitybackend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 public class LoginController {
