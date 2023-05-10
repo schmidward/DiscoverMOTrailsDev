83240f4f48ececfa6fb96b5b49797e84a5767724
Eric D. Schmid <eric.d.schmid1@gmail.com>
2023-05-10 09:46:40 -0500

    more work on the login but still unsuccessful.

--- src/pages/Login/login.js
+++ src/pages/Login/login.js
@@ -1,61 +1,72 @@
 import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
+import axios from 'axios';
 import PropTypes from 'prop-types';
 import './login.css';
 
 //TODO: Move this async function to a seperate directory
-async function loginUser(credentials) {
-  console.log(credentials);
-  console.log(JSON.stringify(credentials));
-  return fetch('http://localhost:8080/login', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json'},
-    body: JSON.stringify(credentials)
-  })
-  .then(data => data.json())
-}
+// async function loginUser(credentials) {
+//   console.log(credentials);
+//   console.log(JSON.stringify(credentials));
+//   return fetch('http://localhost:8080/login', {
+//     method: 'POST',
+//     headers: { 'Content-Type': 'application/json'},
+//     body: JSON.stringify(credentials)
+//   })
+//   .then(data => data.json())
+// }
 
-export default function Login({ setToken }) {
+export default function Login() {
   const [username, setUserName] = useState();
   const [password, setPassword] = useState();
 
-  const handleSubmit = async e => {
-    e.preventDefault();
-    const token = await loginUser({
-      username,
-      password
-    });
-    setToken(token);
-  }
-
-  // const {
-  //   // register,
-  //   handleSubmit,
-  //   // formState: { errors },
-  // } = useForm();
-
-  // const onSubmit = (data) => {
-  //   fetch('http://localhost:8080/login', {
-  //       method: 'POST',
-  //       headers: { 'Content-Type' : 'application/json'},
-  //       body: JSON.stringify({ username: data.username, Credential: data.pwd })
-  //   })
-  //   .then(response => response.text())
-  //   .then(data => console.log(data))
-  //   .catch(error => console.log(error));
+  // const handleSubmit = async e => {
+  //   e.preventDefault();
+  //   const token = await loginUser({
+  //     username,
+  //     password
+  //   });
+  //   setToken(token);
   // }
+
+  const {
+    register,
+    handleSubmit,
+    // formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+    var basicAuth = 'Basic ' + btoa(data.username + ':' + data.password);
+    console.log(basicAuth);
+    fetch('http://localhost:8080/login', {
+      method: 'POST',
+      headers: {
+        'Authorization': '${basicAuth}'
+    }
+    })
+    .then(response => response.json())
+    .then(json => console.log(json));
+  }
 
   return (
     <div className='login-wrapper'>
-      <form className="form" onSubmit={handleSubmit}>
+      <form className="form" onSubmit={handleSubmit(onSubmit)}>
           <div className="label-wrapper">
               <label htmlFor="username">Email: </label>
-              <input type="text" id="username" onChange={e => setUserName(e.target.value)}/>
+              <input 
+              type="text" 
+              id="username" 
+              {...register("username", { required: true })}
+              />
           </div>
 
           <div className="label-wrapper">
-              <label htmlFor="pwd">Password:</label>
-              <input type="password" id="password" onChange={e => setPassword(e.target.value)} />
+              <label htmlFor="password">Password:</label>
+              <input 
+              type="password" 
+              id="password" 
+              {...register("password", { required: true })} />
           </div>
 
           <input type="submit" value="Log In" />
@@ -64,6 +75,6 @@ export default function Login({ setToken }) {
   );
 }
 
-Login.propTypes = {
-  setToken: PropTypes.func.isRequired
-}
+// Login.propTypes = {
+//   setToken: PropTypes.func.isRequired
+// }
