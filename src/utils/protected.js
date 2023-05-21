62bdb201538649feb8294ad92c9861b559e99091
Eric D. Schmid <eric.d.schmid1@gmail.com>
2023-05-21 12:40:29 -0500

    minor update to protected

--- src/utils/protected.js
+++ src/utils/protected.js
@@ -8,13 +8,12 @@ const Protected = ({ children }) => {
     const {user, loadUser, setUser} = useUserContext();
     console.log(user);
     
+    //TODO: Clean up this use effect to only load a user if it's in local storage
     useEffect(() => {
         const storedUser = loadUser();
         console.log(storedUser);
         setUser(storedUser);
     }, []);
-    
-    // authCheck();
 
     return <>{user.isLoggedIn ? children : <Login />}</>;
     
